d6d25f1a7eeedb2381beace8b77918e36c018e14
Queli Coto <quelicm@gmail.com>
2024-07-27 20:16:10 +0200

    ✨PR9 tests auto merge in stage

--- apps/blog/src/components/Counter.tsx
+++ apps/blog/src/components/Counter.tsx
@@ -25,7 +25,7 @@ export const Counter: React.FC<CounterProps> = ({
 				<p>{isBlank("") ? "package isBlank works!" : "isBlank does not work!"}</p>
 			</div>
 			<div className="counter-message">{children}</div>
-			<p>Prueba PR: 9</p>
+			<p>Prueba PR: 10</p>
 		</>
 	);
 }
